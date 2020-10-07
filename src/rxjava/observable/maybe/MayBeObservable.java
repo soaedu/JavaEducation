35b0286c3f57cc532fcf0b1c0cd98f9308fb7ad0
soaedu <sergey.ostrovsky.edu@gmail.com>
2020-10-07 18:16:51 +0200

    Add rxjava observable type example code

---
+++ src/rxjava/observable/maybe/MayBeObservable.java
@@ -0,0 +1,38 @@
+package rxjava.observable.maybe;
+
+import io.reactivex.Maybe;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableMaybeObserver;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+public class MayBeObservable {
+    public static void main(String[] args) throws InterruptedException {
+        //Create an observer
+        Disposable disposable = Maybe.just("MayBe Observable")
+                .delay(2, TimeUnit.SECONDS, Schedulers.io())
+                .subscribeWith(new DisposableMaybeObserver<String>() {
+                                   @Override
+                                   public void onSuccess(@NonNull String value) {
+                                       System.out.println(value);
+                                   }
+
+                                   @Override
+                                   public void onError(@NonNull Throwable throwable) {
+                                       throwable.printStackTrace();
+                                   }
+
+                                   @Override
+                                   public void onComplete() {
+                                       System.out.println("MayBe Completed");
+                                   }
+                               }
+                );
+        Thread.sleep(3000);
+
+        //start observing
+        disposable.dispose();
+    }
+}
